e74d58e04faf358faebd7db9931f1e68d43b3866
Ayo <Ayoroq@gmail.com>
2025-09-02 09:38:01 -0400

    add merge sort function using recursive methods

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ function fibsRec(n) {
     let sequence = fibsRec(n - 1);
     return sequence.concat([sequence[sequence.length - 1] + sequence[sequence.length - 2]]);
 }
+
+
+
+// This is the merge sort function that is using recursive methods
+function mergeSort(arr) {
+    function merge(left, right) {
+        let sorted = [];
+        while (left.length && right.length) {
+            if (left[0] < right[0]) {
+                sorted.push(left.shift());
+            } else {
+                sorted.push(right.shift());
+            }
+        }
+        return [...sorted, ...left, ...right];
+    }
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    return merge(mergeSort(right), mergeSort(left));
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
